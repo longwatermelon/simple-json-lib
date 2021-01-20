8b56b8ac64f9f62e171d26d67e9e553374026a84
longwatermelon <qianhansenb@gmail.com>
2021-01-20 14:46:13 -0700

    path update again

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	std::map<std::string, std::string> dict = json::load_json<std::string, std::string>("../jsontest.json");
+	std::map<std::string, std::string> dict = json::load_json<std::string, std::string>("jsontest.json");
 	for (auto const& [key, val] : dict)
 	{
 		std::cout << key << ':' << val << "\n";
