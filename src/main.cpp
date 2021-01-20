ce21cb5a199efadcf716d8ac02c710c98038a355
longwatermelon <73869536+longwatermelon@users.noreply.github.com>
2021-01-20 14:42:38 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	std::map<std::string, std::string> dict = json::load_json<std::string, std::string>("C:\\Users\\qianh\\OneDrive\\Documents\\code\\cpp\\json-lib\\jsontest.json");
+	std::map<std::string, std::string> dict = json::load_json<std::string, std::string>("../jsontest.json");
 	for (auto const& [key, val] : dict)
 	{
 		std::cout << key << ':' << val << "\n";
