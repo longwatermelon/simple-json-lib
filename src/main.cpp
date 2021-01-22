029cd9fb90e8dafff3fc003291ec7842b386539e
longwatermelon <73869536+longwatermelon@users.noreply.github.com>
2021-01-21 18:52:54 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@ int main()
 {
 	// example code
 	std::string path = "jsontest.json";
-	/*std::map<std::string, std::string> dict = json::load<std::string, std::string>(path);
+	std::map<std::string, std::string> dict = json::load<std::string, std::string>(path);
 	dict["pee"] = "modified value";
-	json::dump<std::string, std::string>(path, dict);*/
+	json::dump<std::string, std::string>(path, dict);
 
 	std::map<std::string, int> new_dict{
 		{"5", 3},
